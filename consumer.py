ef971c3fa234c78ba16fc12916804d8f7d043e87
Martin Forstner <martin@redislabs.com>
2020-01-12 13:24:19 +0100

    handle interrupt errors

--- consumer.py
+++ consumer.py
@@ -8,17 +8,23 @@ r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
 
 while True:
 
-    buffer = r.xread({STREAM_NAME: '$'}, block=1000, count=1)
+    try:
 
-    if not buffer:
-        log('could not read from stream')
-        break
+        buffer = r.xread({STREAM_NAME: '$'}, block=1000, count=1)
 
-    frame = cv2.imdecode(numpy.frombuffer(buffer[0][1][0][1][ENTRY_KEY], numpy.uint8), 1)
+        if not buffer:
+            log('could not read from stream')
+            break
 
-    cv2.imshow('frame', frame)
+        frame = cv2.imdecode(numpy.frombuffer(buffer[0][1][0][1][ENTRY_KEY], numpy.uint8), 1)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            log('quitting ...')
+            break
+
+    except (KeyboardInterrupt, InterruptedError):
         log('quitting ...')
         break
 
